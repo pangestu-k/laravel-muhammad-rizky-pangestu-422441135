326a2528c2074c4bf757355cda4d93966bd2f450
Pangestu-k <risky.mpangestu@gmail.com>
2025-11-12 14:59:25 +0700

    feat: Menambahkan Migration table sesuai kebutuhan modul

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->string('password', 255);
+            $table->enum('role', ['admin', 'customer']);
+            $table->string('alamat', 255);
+            $table->string('telepon', 20);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2025_11_12_074629_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->date('tanggal');
+            $table->decimal('total', 10, 2);
+            $table->string('bukti_pembayaran', 255);
+            $table->enum('status_pembayaran', ['pending', 'sukses', 'gagal']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
